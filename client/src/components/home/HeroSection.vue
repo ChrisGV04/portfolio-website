4552035b72ee1b8f0e62108a7df4235ec5ff1e0e
Christian Gil <christian@chrisgvdev.com>
2022-12-17 18:50:08 -0600

    client: Use bigger BaseContainer paddings

--- client/src/components/home/HeroSection.vue
+++ client/src/components/home/HeroSection.vue
@@ -7,14 +7,14 @@
     />
 
     <div class="absolute inset-0 -z-10">
-      <BaseContainer class="grid grid-cols-4 gap-5">
+      <BaseContainer no-y-padding class="grid grid-cols-4 gap-5">
         <div v-for="col in 4" class="flex flex-col space-y-5">
           <div v-for="img in 3" class="aspect-long-img-v w-full rounded-2xl bg-gray-200"></div>
         </div>
       </BaseContainer>
     </div>
 
-    <BaseContainer class="relative flex h-full items-end">
+    <BaseContainer no-y-padding class="relative flex h-full items-end pb-8">
       <div class="z-10 w-full">
         <h1 class="text-center uppercase leading-none text-gray-900">
           <span class="block text-[clamp(1.5rem,4.6vw,4.5rem)]">{{ $t('home.hero.line1') }}</span>
