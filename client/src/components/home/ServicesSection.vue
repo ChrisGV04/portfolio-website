c5946b4e4547b937630ce764866061bbbf04b010
Christian Gil <christian@chrisgvdev.com>
2023-01-02 16:11:44 -0600

    client: Make the ServicesSection responsive

--- client/src/components/home/ServicesSection.vue
+++ client/src/components/home/ServicesSection.vue
@@ -27,25 +27,29 @@ const services: ServiceItem[] = [
 </script>
 
 <template>
-  <section>
-    <BaseContainer>
+  <section class="py-24 lg:py-32">
+    <BaseContainer no-y-padding>
       <div class="flex flex-col">
-        <h2 class="text-h2 font-medium uppercase text-gray-900">
-          {{ $t('home.services.title') }}
-        </h2>
-        <span class="mt-4 text-xl text-gray-700">{{ $t('home.services.subtext') }}</span>
+        <h2 class="text-h2 font-medium uppercase text-gray-900">{{ $t('home.services.title') }}</h2>
+        <span class="mt-4 text-body text-gray-700">{{ $t('home.services.subtext') }}</span>
       </div>
+    </BaseContainer>
 
-      <ul class="mt-20 grid grid-cols-3 gap-5">
-        <li v-for="service in services" class="w-full">
-          <div class="aspect-4-5 w-full rounded-2xl bg-gray-300"></div>
+    <BaseContainer
+      no-y-padding
+      class="mt-20 flex snap-x gap-5 overflow-x-auto md:grid md:grid-cols-3 md:overflow-hidden"
+    >
+      <div
+        v-for="service in services"
+        class="w-[calc(100vw-4rem)] max-w-[25rem] shrink-0 md:w-full md:max-w-none"
+      >
+        <div class="aspect-4-5 w-full rounded-2xl bg-gray-300"></div>
 
-          <div class="mt-4 flex flex-col">
-            <h3 class="text-2xl font-medium text-gray-900">{{ service.name }}</h3>
-            <span class="text-gray-500">{{ service.cat1 }} &middot; {{ service.cat2 }}</span>
-          </div>
-        </li>
-      </ul>
+        <div class="mt-4 flex flex-col">
+          <h3 class="text-2xl font-medium text-gray-900">{{ service.name }}</h3>
+          <span class="text-gray-500">{{ service.cat1 }} &middot; {{ service.cat2 }}</span>
+        </div>
+      </div>
     </BaseContainer>
   </section>
 </template>
