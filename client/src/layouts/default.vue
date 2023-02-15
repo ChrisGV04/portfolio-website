79eb59f35a8eddd82bfa7958e00b04459a93b55a
Christian Gil <christian@chrisgvdev.com>
2023-02-14 20:29:08 -0600

    client: Fix layout shifting

--- client/src/layouts/default.vue
+++ client/src/layouts/default.vue
@@ -7,31 +7,35 @@ function transitionBL() {
 function transitionAE() {
   transitionStyles.value = 'opacity-100 translate-y-full md:translate-y-0 md:translate-x-full';
 }
+
+const { locale } = useI18n();
 </script>
 
 <template>
-  <Html>
-    <Body class="antialiased transition-colors">
-      <GlobalHeader />
-      <NuxtPage
-        :transition="{
-          duration: 300,
-          mode: 'out-in',
-          onAfterEnter: transitionAE,
-          onBeforeLeave: transitionBL,
-        }"
-      />
-      <GlobalFooter />
+  <div class="flex min-h-screen flex-col">
+    <Html :lang="locale" />
+    <Body class="antialiased transition-colors" />
 
-      <Teleport to="body">
-        <div
-          id="transition-bar"
-          :class="[
-            'fixed top-0 right-0 z-50 h-screen w-screen transform bg-gray-800 transition-transform duration-300',
-            transitionStyles,
-          ]"
-        ></div>
-      </Teleport>
-    </Body>
-  </Html>
+    <GlobalHeader />
+    <NuxtPage
+      class="flex-1"
+      :transition="{
+        duration: 300,
+        mode: 'out-in',
+        onAfterEnter: transitionAE,
+        onBeforeLeave: transitionBL,
+      }"
+    />
+    <GlobalFooter />
+
+    <Teleport to="body">
+      <div
+        id="transition-bar"
+        :class="[
+          'fixed top-0 right-0 z-50 h-screen w-screen transform bg-gray-800 transition-transform duration-300',
+          transitionStyles,
+        ]"
+      ></div>
+    </Teleport>
+  </div>
 </template>
