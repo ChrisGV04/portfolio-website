88cc0c5e3bb258aa0cc728c3abe4cef66acfdc0d
Christian Gil <christian@chrisgvdev.com>
2023-02-15 20:58:52 -0600

    client: Add info to the projects page

--- client/src/pages/projects/index.vue
+++ client/src/pages/projects/index.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
-const { t } = useI18n();
+import type { Paginated, Project } from '~/types/cms';
+
+const { t, locale } = useI18n();
 useHead({ title: t('pageTitles.projects') });
+
+const env = useRuntimeConfig();
+const { data } = await useAsyncData(async () => {
+  const { docs } = await $fetch<Paginated<Project>>(`${env.public.apiUrl}/projects?locale=${locale.value}`);
+  return docs;
+});
+
+console.log(data.value);
 </script>
 
 <template>
-  <main>
-    <Body class="bg-gray-50" />
-    <h1>{{ t('navigation.projects') }}</h1>
+  <main v-if="data?.length">
+    <HomePortfolioSection :projects="data" />
   </main>
 </template>
