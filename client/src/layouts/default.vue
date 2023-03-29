9c36217f9491f2e6086e93a338ca53ab3606e470
Christian Gil <christian@chrisgvdev.com>
2023-03-28 22:06:58 -0600

    client: Reset the layout

--- client/src/layouts/default.vue
+++ client/src/layouts/default.vue
@@ -1,13 +1,4 @@
 <script setup lang="ts">
-const transitionStyles = ref('opacity-0 translate-y-full md:translate-y-0 md:translate-x-full');
-function transitionBL() {
-  transitionStyles.value = 'opacity-100 translate-y-0 md:translate-x-0';
-}
-
-function transitionAE() {
-  transitionStyles.value = 'opacity-100 translate-y-full md:translate-y-0 md:translate-x-full';
-}
-
 const { locale } = useI18n();
 </script>
 
@@ -16,26 +7,6 @@ const { locale } = useI18n();
     <Html :lang="locale" />
     <Body class="antialiased transition-colors" />
 
-    <GlobalHeader />
-    <NuxtPage
-      class="flex-1"
-      :transition="{
-        duration: 300,
-        mode: 'out-in',
-        onAfterEnter: transitionAE,
-        onBeforeLeave: transitionBL,
-      }"
-    />
-    <GlobalFooter />
-
-    <Teleport to="body">
-      <div
-        id="transition-bar"
-        :class="[
-          'fixed top-0 right-0 z-50 h-screen w-screen transform bg-gray-800 transition-transform duration-300',
-          transitionStyles,
-        ]"
-      ></div>
-    </Teleport>
+    <NuxtPage class="flex-1" />
   </div>
 </template>
