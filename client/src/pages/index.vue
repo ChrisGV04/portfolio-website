9001694a6e3ba2d04bffaf306da44043774c0959
Christian Gil <christian@chrisgvdev.com>
2023-03-30 12:30:57 -0600

    client: Implement the BaseMarquee on the home page

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -8,5 +8,16 @@ useHead({ title: t('pageTitles.home') });
     <HomeHeroSection />
     <HomeAboutSection />
     <HomePortfolioSection />
+
+    <section class="pb-24">
+      <BaseMarquee :speed="40">
+        <div class="ml-4 flex flex-nowrap gap-4 md:ml-8 md:gap-8">
+          <div
+            v-for="img in 5"
+            class="aspect-[4/3] w-72 shrink-0 rounded-3xl bg-gray-600 md:w-80 lg:w-[27vw]"
+          ></div>
+        </div>
+      </BaseMarquee>
+    </section>
   </main>
 </template>
